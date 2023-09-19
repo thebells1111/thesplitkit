56b9a4a0d0a71791680951eb713449fcaeb05386
Steven Bell <dev.thebells1111@gmail.com>
2023-09-19 00:04:07 -0500

    adding easier event creation

---
+++ src/lib/Modal/SmallModal.svelte
@@ -0,0 +1,69 @@
+<script>
+	import Close from '$lib/icons/Close.svelte';
+	export let showModal;
+	export let unsaved = false;
+	export let onClose = () => {};
+	function closeModal() {
+		onClose();
+		if (unsaved) {
+			const userConfirmation = window.confirm('Your changes are unsaved. Do you want to continue?');
+			if (userConfirmation) {
+				showModal = false;
+				unsaved = false;
+			}
+		} else {
+			showModal = false;
+		}
+	}
+</script>
+
+<blurred-background on:mousedown|self={closeModal} on:touchend|self={closeModal}>
+	<modal>
+		<button class="close icon" on:click={closeModal}>
+			<Close size="24" />
+		</button>
+		<modal-content>
+			<slot />
+		</modal-content>
+	</modal>
+</blurred-background>
+
+<style>
+	blurred-background {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 100%;
+		position: absolute;
+		background: transparent;
+		top: 0;
+		right: 0;
+		z-index: 99;
+		backdrop-filter: blur(5px);
+	}
+
+	modal {
+		display: block;
+		position: relative;
+		width: calc(100% - 56px);
+		max-width: 991px;
+		overflow-y: auto;
+		overflow-x: hidden;
+		border-radius: 8px;
+		background-color: white;
+		padding: 40px 20px 20px 20px;
+		box-shadow: 0px 3px 10px 3px rgba(0, 0, 0, 1);
+	}
+
+	.close {
+		position: absolute;
+		top: 0;
+		right: 0;
+		background-color: transparent;
+		padding: 8px;
+		color: rgba(0, 0, 0, 0.75);
+		z-index: 33;
+		border: 1px solid transparent;
+	}
+</style>
--- src/routes/(main)/events/creator/+page.svelte
+++ src/routes/(main)/events/creator/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Event from '$lib/icons/Event.svelte';
+	import SmallModal from '$lib/Modal/SmallModal.svelte';
 	import { remoteServer } from '$/stores';
 	import { goto } from '$app/navigation';
 	let eventName = '';
@@ -17,6 +18,26 @@
 			goto('/events/dashboard/' + data.guid);
 		}
 	}
+
+	let showHelpModal = false;
+	let helpText = ``;
+
+	function showHelp(type) {
+		showHelpModal = true;
+		helpText = texts[type];
+	}
+
+	const texts = {
+		manual: `<p>In manual, there's no auto switching of the blocks.</p>
+		<p>
+			It's useful for live events like a conference or debate where there's someone available to
+			quickly update the blocks as the speaker changes or an image needs to be displayed.
+		</p>
+		<p>
+			It could also be used by a band doing a live set by having a Split Kit engineer changing the
+			block to match the song being played so the audience can boost the song live.
+		</p>`
+	};
 </script>
 
 <div>
@@ -24,21 +45,48 @@
 		Event Name:
 		<input bind:value={eventName} />
 	</label>
-
-	<button-container>
-		<button on:click={generateLink}>
-			<Event size="32" />
-		</button>
-		<p>Create Event</p>
-	</button-container>
 </div>
+
+<h1>Event Type</h1>
+
+<button-container>
+	<button class="selector" on:click={generateLink}> <h2>Music Show</h2> </button>
+	<button class="help" on:click={showHelp.bind(this, 'manual')}>?</button>
+</button-container>
+<button-container>
+	<button class="selector" on:click={generateLink}> <h2>Podcast</h2> </button>
+	<button class="help" on:click={showHelp.bind(this, 'manual')}>?</button>
+</button-container>
+<button-container>
+	<button class="selector" on:click={generateLink}> <h2>Playlist</h2> </button>
+	<button class="help" on:click={showHelp.bind(this, 'manual')}>?</button>
+</button-container>
+<button-container>
+	<button class="selector" on:click={generateLink}> <h2>Prerecorded Show</h2> </button>
+	<button class="help" on:click={showHelp.bind(this, 'manual')}>?</button>
+</button-container>
+<button-container>
+	<button class="selector" on:click={generateLink}> <h2>Manual</h2> </button>
+	<button class="help" on:click={showHelp.bind(this, 'manual')}>?</button>
+</button-container>
+
+{#if showHelpModal}
+	<SmallModal
+		bind:showModal={showHelpModal}
+		onClose={() => {
+			helpText = '';
+		}}
+	>
+		{@html helpText}
+	</SmallModal>
+{/if}
 
 <style>
 	div {
 		width: calc(100% - 16px);
 		max-width: 450px;
 		display: flex;
-		margin: 100px auto;
+		margin: 16px auto 0 auto;
 		position: relative;
 	}
 	label {
@@ -47,27 +95,47 @@
 		margin-bottom: 16px;
 		width: 100%;
 	}
-	button {
+	button.selector {
 		background-color: hsl(0, 0%, 96%);
 		color: var(--color-text-0);
 		display: flex;
 		align-items: center;
 		justify-content: center;
 		height: 50px;
-		width: 50px;
+		width: 100%;
 		border-radius: 40px;
 		padding: 0;
-		margin: 0 0 4px 0;
+		margin: 0 8px 4px 0;
 		box-shadow: 0 2px 4px 1px rgba(0, 0, 0, 0.5);
+	}
+
+	button.help {
+		background-color: var(--color-theme-yellow);
+		color: var(--color-text-0);
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: 50px;
+		min-width: 50px;
+		border-radius: 40px;
+		padding: 0;
+		margin: 0 8px 4px 0;
+		box-shadow: 0 2px 4px 1px rgba(0, 0, 0, 0.5);
+		font-size: 1.2em;
+		font-weight: 550;
 	}
 	button-container {
 		display: flex;
-		flex-direction: column;
 		align-items: center;
 		justify-content: center;
-		width: 75px;
+		padding: 8px;
 		position: relative;
 		bottom: 8px;
+	}
+
+	h1 {
+		text-align: center;
+		text-decoration: underline;
 	}
 	p {
 		text-align: center;
